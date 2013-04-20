5e0bdc8af4bff7659d1b41b09fe8f166e0842c36
Sven Gothel <sgothel@jausoft.com>
2013-04-20 23:24:20 +0200

    Add toolchain.gcc-x86_32.cmake for x86_64 -> x86_32 crosscompilation

---
+++ cmake/toolchain.gcc-x86_32.cmake
@@ -0,0 +1,17 @@
+# For multiarch crosscompile, i.e. x86_32 target on x86_64 host
+
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+link_directories("/usr/lib32")
+
+set(CMAKE_C_FLAGS "-m32")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
+      
+set(LINKER_FLAGS "-m32 -static-libgcc")
+
+set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+
