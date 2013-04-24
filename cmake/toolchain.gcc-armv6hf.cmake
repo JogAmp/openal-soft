0df1e1ad30a0990591be4779ccc3d0034a3b33e5
Sven Gothel <sgothel@jausoft.com>
2013-04-25 01:14:57 +0200

    Toolchain CMake files for ARM* Cross-Compilation require host include directory (not in system include path) to pick up pulseaudio/alsa.

--- cmake/toolchain.gcc-armv6hf.cmake
+++ cmake/toolchain.gcc-armv6hf.cmake
@@ -1,6 +1,6 @@
 # For normal gcc compilation, but use static-libgcc
 
-set(CMAKE_C_FLAGS "-fpic -march=armv6 -marm -mfloat-abi=hard -include ${CMAKE_MODULE_PATH}/glibc-compat-symbols.h")
+set(CMAKE_C_FLAGS "-fpic -march=armv6 -marm -mfloat-abi=hard -include ${CMAKE_MODULE_PATH}/glibc-compat-symbols.h -I/usr/include")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
