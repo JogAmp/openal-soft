389ae1f767bfad6116e21306fc3cdf89a4cbcc0a
Sven Gothel <sgothel@jausoft.com>
2013-09-13 19:08:52 +0200

    Add clang cmake.toolchain macros; Fix glibc-compat-symbols.h to work w/ clang (nop)

---
+++ cmake/toolchain.clang-default.cmake
@@ -0,0 +1,13 @@
+# For normal clang compilation
+
+# set(CMAKE_C_FLAGS "-include ${CMAKE_MODULE_PATH}/glibc-compat-symbols.h")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
+
+# set(LINKER_FLAGS "-static-libgcc")
+
+set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
+
