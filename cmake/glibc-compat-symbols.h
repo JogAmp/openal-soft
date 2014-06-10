95a2fe69853d6996cf9e00a9073c57cf9cef9ae9
Sven Gothel <sgothel@jausoft.com>
2014-06-11 00:53:04 +0200

    glibc-compat-symbols.h: Disable warning for not supported GLIBC_COMPAT_SYMBOL, since included even for OSX

--- cmake/glibc-compat-symbols.h
+++ cmake/glibc-compat-symbols.h
@@ -37,7 +37,7 @@
         #define GLIBC_COMPAT_SYMBOL(FFF)
     #endif
 #else
-    #warning GLIBC_COMPAT_SYMBOL not supported with target OS
+    // #warning GLIBC_COMPAT_SYMBOL not supported with target OS
     #define GLIBC_COMPAT_SYMBOL(FFF)
 #endif
 
