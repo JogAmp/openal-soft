a71e2ed35cc57dfc9ca6d33c3a2b8a53b3659710
Sven Gothel <sgothel@jausoft.com>
2014-06-10 19:14:45 +0200

    CMake: Don't use CMAKE_TOOLCHAIN_FILE for non-crosscompilation, use OPENAL_CUSTOM_CONFIG to inject cmake commands

--- cmake/customconfig.clang-default.cmake
+++ cmake/customconfig.clang-default.cmake
@@ -1,16 +1,14 @@
-# For multiarch gcc crosscompile, i.e. x86_32 target on x86_64 host
+# For normal clang compilation
 
-set(CMAKE_SYSTEM_PROCESSOR "x86")
+set(CMAKE_C_FLAGS "-include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
+set(CMAKE_CXX_FLAGS "-include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
 
-link_directories("/usr/lib32")
-
-# set(CMAKE_C_FLAGS "-m32 -include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
-# set(CMAKE_CXX_FLAGS "-m32 -include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
-
+set(CMAKE_CXX_CMPILER "gcc")
+set(CMAKE_C_CMPILER "gcc")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
-      
-# set(LINKER_FLAGS "-m32 -static-libgcc")
+
+# set(LINKER_FLAGS "-static-libgcc")
 
 set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
 set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
--- cmake/customconfig.clang-x86_32.cmake
+++ cmake/customconfig.clang-x86_32.cmake
@@ -1,12 +1,16 @@
-# For normal clang compilation
+# For multiarch gcc crosscompile, i.e. x86_32 target on x86_64 host
 
-# set(CMAKE_C_FLAGS "-include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
-# set(CMAKE_CXX_FLAGS "-include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+link_directories("/usr/lib32")
+
+set(CMAKE_C_FLAGS "-m32 -include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
+set(CMAKE_CXX_FLAGS "-m32 -include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
-
-# set(LINKER_FLAGS "-static-libgcc")
+      
+# set(LINKER_FLAGS "-m32 -static-libgcc")
 
 set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
 set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
