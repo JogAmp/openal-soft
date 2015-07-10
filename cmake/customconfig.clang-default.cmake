178194b7de4b4ad1de0c701356fca46565dde762
Sven Gothel <sgothel@jausoft.com>
2015-07-11 01:07:23 +0200

    cmake/clang: Keep using clang; OSX: -mmacosx-version-min=10.5; Inject additional architectures for fat-binary (macosx)

--- cmake/customconfig.clang-default.cmake
+++ cmake/customconfig.clang-default.cmake
@@ -3,12 +3,27 @@
 set(CMAKE_C_FLAGS "-include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
 set(CMAKE_CXX_FLAGS "-include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
 
-set(CMAKE_CXX_CMPILER "gcc")
-set(CMAKE_C_CMPILER "gcc")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
+if(APPLE)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.5")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.5")
+endif()
+
+# inject additional architectures for fat-binary (macosx)
+# CMAKE_EXTRA_ARCHS is set from JOAL's ant build.xml
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CMAKE_EXTRA_ARCHS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_EXTRA_ARCHS}")
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
+
+message(STATUS "CMAKE_C_COMPILER  : ${CMAKE_C_COMPILER}")
+message(STATUS "CMAKE_CXX_COMPILER: ${CMAKE_CXX_COMPILER}")
+message(STATUS "CMAKE_C_FLAGS  : ${CMAKE_C_FLAGS}")
+message(STATUS "CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
 
 # set(LINKER_FLAGS "-static-libgcc")
+
+message(STATUS "LINKER_FLAGS: ${LINKER_FLAGS}")
 
 set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
 set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
--- cmake/customconfig.clang-x86_32.cmake
+++ cmake/customconfig.clang-x86_32.cmake
@@ -1,4 +1,4 @@
-# For multiarch gcc crosscompile, i.e. x86_32 target on x86_64 host
+# For multiarch clang crosscompile, i.e. x86_32 target on x86_64 host
 
 set(CMAKE_SYSTEM_PROCESSOR "x86")
 
@@ -7,10 +7,22 @@ link_directories("/usr/lib32")
 set(CMAKE_C_FLAGS "-m32 -include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
 set(CMAKE_CXX_FLAGS "-m32 -include ${PROJECT_SOURCE_DIR}/cmake/glibc-compat-symbols.h")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c++ flags")
+if(APPLE)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.5")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mmacosx-version-min=10.5")
+endif()
+
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
       
+message(STATUS "CMAKE_C_COMPILER  : ${CMAKE_C_COMPILER}")
+message(STATUS "CMAKE_CXX_COMPILER: ${CMAKE_CXX_COMPILER}")
+message(STATUS "CMAKE_C_FLAGS  : ${CMAKE_C_FLAGS}")
+message(STATUS "CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}")
+
 # set(LINKER_FLAGS "-m32 -static-libgcc")
+
+message(STATUS "LINKER_FLAGS: ${LINKER_FLAGS}")
 
 set(CMAKE_SHARED_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
 set(CMAKE_MODULE_LINKER_FLAGS "${LINKER_FLAGS}" CACHE STRING "linker flags" FORCE)
